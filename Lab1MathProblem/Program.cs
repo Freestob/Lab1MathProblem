e92bad936418fd3ea79d1de25effd19d427764a1
Freestob <bradley.freestone@gmail.com>
2018-06-14 17:07:42 -0500

    Added function to check if user entry was a number and to ask again if it wasn't

--- Lab1MathProblem/Program.cs
+++ Lab1MathProblem/Program.cs
@@ -12,57 +12,70 @@ namespace Lab1MathProblem
         {   // This is the string holding what the users input into the console prompt.
             string entryOne, entryTwo;
 
-            // This section takes what the user has entered for the numbers and displays them back to the user.
-            Console.WriteLine("Please enter the first number");
-            entryOne = Console.ReadLine();
+            /*  // This section takes what the user has entered for the numbers and displays them back to the user.
+              Console.WriteLine("Please enter the first number");
+              entryOne = Console.ReadLine();
 
-            Console.WriteLine("Please enter the second number");
-            entryTwo = Console.ReadLine();
+              Console.WriteLine("Please enter the second number");
+              entryTwo = Console.ReadLine();
 
-            Console.WriteLine("The numbers you entered are {0} and {1}.", entryOne, entryTwo);
+              Console.WriteLine("The numbers you entered are {0} and {1}.", entryOne, entryTwo);
 
-            //This portion converts the numbers the user entered to actual numbers.
+              //This portion converts the numbers the user entered to actual numbers.
 
-            var blockNumberOne = entryOne;
-            var blockNumberTwo = entryTwo;
+              var blockNumberOne = entryOne;
+              var blockNumberTwo = entryTwo;
 
-            //This portion selects the individual numbers in the var
+              //This portion selects the individual numbers in the var
 
-            char cOneOne = entryOne[0];
-            char cOneTwo = entryOne[1];
-            char cOneThree = entryOne[2];
+              char cOneOne = entryOne[0];
+              char cOneTwo = entryOne[1];
+              char cOneThree = entryOne[2];
 
-            char cTwoOne = entryTwo[0];
-            char cTwoTwo = entryTwo[1];
-            char cTwoThree = entryTwo[2];
+              char cTwoOne = entryTwo[0];
+              char cTwoTwo = entryTwo[1];
+              char cTwoThree = entryTwo[2];
 
-            // This  portion converts the chars into ints
+              // This  portion converts the chars into ints
 
-            var iOneOne = cOneOne - '0';
-            var iOneTwo = cOneTwo - '0';
-            var iOneThree = cOneThree - '0';
+              var iOneOne = cOneOne - '0';
+              var iOneTwo = cOneTwo - '0';
+              var iOneThree = cOneThree - '0';
 
-            var iTwoOne = cTwoOne - '0';
-            var iTwoTwo = cTwoTwo - '0';
-            var iTwoThree = cTwoThree - '0';
+              var iTwoOne = cTwoOne - '0';
+              var iTwoTwo = cTwoTwo - '0';
+              var iTwoThree = cTwoThree - '0';
 
-            Console.WriteLine(iOneOne+iTwoTwo);
+              // This simplifies the above variables to for the if else statement to save me some typing.
 
+              var oneAdd = iOneOne + iTwoOne;
+              var twoAdd = iOneTwo + iTwoTwo;
+              var threeAdd = iOneThree + iTwoThree; */
+            int vEntryOne = GetNumberInput();
+            int vEntryTwo = GetNumberInput();
 
-                
-            
+            Console.WriteLine(vEntryOne + vEntryTwo);
+        }
+        static int GetNumberInput()
+        {
+            string userInput;
+            int userNumber;
 
-           
-        
-            
+            Console.WriteLine("Please input a number.");
+            userInput = Console.ReadLine();
+            bool success = int.TryParse(userInput, out userNumber);
 
-
-
-
-
+            if (success == true)
+            {
+                return userNumber;
+            }
+            else
+            {
+                Console.WriteLine("Please input a valid number.");
+                return GetNumberInput();
+            }
 
         }
-
 
 
 
