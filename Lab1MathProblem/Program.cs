9c0ad0ae6d7769cb37ed75d02961a2a362f3e394
Freestob <bradley.freestone@gmail.com>
2018-06-19 19:33:25 -0500

    Started an array to separate number into digits

--- Lab1MathProblem/Program.cs
+++ Lab1MathProblem/Program.cs
@@ -3,60 +3,20 @@
 namespace Lab1MathProblem
 {
     class Program
-    {
-        static void Main(string[] args)
-
+    {   
         // The purpose of this program is to Check if each corresponding place in the two numbers (ones, tens, hundreds, …)
-        // sums to the same total.        
-
-        {   // This is the string holding what the users input into the console prompt.
-            string entryOne, entryTwo;
-
-            /*  // This section takes what the user has entered for the numbers and displays them back to the user.
-              Console.WriteLine("Please enter the first number");
-              entryOne = Console.ReadLine();
-
-              Console.WriteLine("Please enter the second number");
-              entryTwo = Console.ReadLine();
-
-              Console.WriteLine("The numbers you entered are {0} and {1}.", entryOne, entryTwo);
-
-              //This portion converts the numbers the user entered to actual numbers.
-
-              var blockNumberOne = entryOne;
-              var blockNumberTwo = entryTwo;
-
-              //This portion selects the individual numbers in the var
-
-              char cOneOne = entryOne[0];
-              char cOneTwo = entryOne[1];
-              char cOneThree = entryOne[2];
-
-              char cTwoOne = entryTwo[0];
-              char cTwoTwo = entryTwo[1];
-              char cTwoThree = entryTwo[2];
-
-              // This  portion converts the chars into ints
-
-              var iOneOne = cOneOne - '0';
-              var iOneTwo = cOneTwo - '0';
-              var iOneThree = cOneThree - '0';
-
-              var iTwoOne = cTwoOne - '0';
-              var iTwoTwo = cTwoTwo - '0';
-              var iTwoThree = cTwoThree - '0';
-
-              // This simplifies the above variables to for the if else statement to save me some typing.
-
-              var oneAdd = iOneOne + iTwoOne;
-              var twoAdd = iOneTwo + iTwoTwo;
-              var threeAdd = iOneThree + iTwoThree; */
+        // sums to the same total.   
+        static void Main(string[] args)
+        {   
+            // This portion of code calls upon the functoin to assign the value to the two ints
+            
             int vEntryOne = GetNumberInput();
             int vEntryTwo = GetNumberInput();
 
             Console.WriteLine(vEntryOne + vEntryTwo);
+            
         }
-        static int GetNumberInput()
+            static int GetNumberInput()
         {
             string userInput;
             int userNumber;
@@ -76,6 +36,7 @@ namespace Lab1MathProblem
             }
 
         }
+            
 
 
 
