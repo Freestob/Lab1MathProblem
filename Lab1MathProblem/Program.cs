f1e00ae9ca7b5e9197ffe2d154ab791bd389c883
Freestob <bradley.freestone@gmail.com>
2018-06-19 20:07:58 -0500

    Completed the function to convert the user entered number into an array

--- Lab1MathProblem/Program.cs
+++ Lab1MathProblem/Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab1MathProblem
 {
     class Program
-    {   
+    {
         // The purpose of this program is to Check if each corresponding place in the two numbers (ones, tens, hundreds, …)
         // sums to the same total.   
         static void Main(string[] args)
-        {   
+        {
             // This portion of code calls upon the functoin to assign the value to the two ints
-            
+
             int vEntryOne = GetNumberInput();
             int vEntryTwo = GetNumberInput();
 
             Console.WriteLine(vEntryOne + vEntryTwo);
-            
+
+            int[] arrayOne = UserNumberToArray(vEntryOne);
+            int[] arrayTwo = UserNumberToArray(vEntryTwo);
+            Console.WriteLine(arrayOne[0]);
+            Console.WriteLine(arrayTwo[0]);
         }
-            static int GetNumberInput()
+        static int GetNumberInput()
         {
             string userInput;
             int userNumber;
@@ -36,8 +41,23 @@ namespace Lab1MathProblem
             }
 
         }
-            
+        // This function takes the user entered number and separates it into an array.
 
+        static int[] UserNumberToArray(int number)
+        {
+            // The number runs through a loop that ends when the number eventually equals zero. While running through the loop the
+            // function saves the remainder of each number when it is divided by 10. It then sets the new value of the number to 
+            // be equal to itselve divded by 10 and removing the remainder. This works because int does not store numbers in the 
+            // decimal place.
+
+            List<int> numberArray = new List<int>();
+            while(number > 0)
+            {
+                numberArray.Add(number % 10);
+                number /= 10;
+            }
+            return new int[0];
+        }
 
 
 
